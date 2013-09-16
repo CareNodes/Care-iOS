6911405118965ee1ceeb46914dc2fe2b602bf351
0day2010 <0day.zh@gmail.com>
2013-09-17 01:03:45 +0800

    ADD home, touch, me viewController.

---
+++ Care/Care/CNHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CNHomeViewController.h
+//  Care
+//
+//  Created by 0day on 13-9-17.
+//  Copyright (c) 2013年 CareNodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNHomeViewController : UITableViewController
+
+@end
---
+++ Care/Care/CNTouchViewController.h
@@ -0,0 +1,13 @@
+//
+//  CNTouchViewController.h
+//  Care
+//
+//  Created by 0day on 13-9-17.
+//  Copyright (c) 2013年 CareNodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNTouchViewController : UITableViewController
+
+@end
